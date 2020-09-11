1e6797f445dd606af6d6ad40915d224f4e5028b6
Freddy Monroy <fredydeltoro@gmail.com>
2020-09-11 00:31:37 -0500

    Displaying recipes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
   get 'homepage/index'
   root 'homepage#index'
-  # get '/*path' => 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
