b056e9914d886a975af06704ff20911f6a32c1f1
Freddy Monroy <fredydeltoro@gmail.com>
2020-09-13 18:27:38 -0500

    Change to background to prove heroku deploy

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center" style={{background: '#69778C'}}>
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
+        <h1 className="display-4 text-white">Food Recipes</h1>
         <p className="lead">
           A curated list of recipes for the best homemade meal and delicacies.
         </p>
-        <hr className="my-4" />
+        <hr className="my-4 text-white" style={{ borderTop: '1px solid '}} />
         <Link
           to="/recipes"
           className="btn btn-lg custom-button"
